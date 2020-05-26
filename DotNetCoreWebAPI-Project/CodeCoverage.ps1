002706dda48a9b17ae3f65f55881f5ba67726c7f
Vilas Rajbhar <vilas.rajbhar89@gmail.com>
2020-05-26 17:09:38 +0530

    Reqno10: Powershell command changes done.

--- DotNetCoreWebAPI-Project/CodeCoverage.ps1
+++ DotNetCoreWebAPI-Project/CodeCoverage.ps1
@@ -1,32 +1,28 @@
-﻿cd .\DotNetCoreWebAPI-Project
+﻿    dotnet test .\web-api.sln --settings:.\CodeCoverage.runsettings
 
-pwd
+    $recentCoverageFile = Get-ChildItem -File -Filter *.coverage -Path .\web-api-tests\TestResults -Name -Recurse | Select-Object -First 1;
 
-
-dotnet build .\web-api.sln
-
-dotnet test .\web-api.sln
-
-$recentCoverageFile = Get-ChildItem -File -Filter *.coverage -Path .\web-api-tests\TestResults -Name -Recurse | Select-Object -First 1;
-
-write-host 'Test Completed'  -ForegroundColor Green
+    write-host 'Test Completed'  -ForegroundColor Green
 
 
-C:\Users\vilas\.nuget\packages\microsoft.codecoverage\16.6.1\build\netstandard1.0\CodeCoverage\CodeCoverage.exe analyze  /output:.\web-api-tests\TestResults\MyTestOutput.coveragexml  .\web-api-tests\TestResults'\'$recentCoverageFile
+    C:\Users\vilas\.nuget\packages\microsoft.codecoverage\16.6.1\build\netstandard1.0\CodeCoverage\CodeCoverage.exe analyze  /output:.\web-api-tests\TestResults\MyTestOutput.coveragexml  .\web-api-tests\TestResults'\'$recentCoverageFile
 
-write-host 'CoverageXML Generated'  -ForegroundColor Green
+    write-host 'CoverageXML Generated'  -ForegroundColor Green
+
+    
+    dotnet C:\tools\.store\dotnet-reportgenerator-globaltool\4.5.8\dotnet-reportgenerator-globaltool\4.5.8\tools\netcoreapp3.0\any\ReportGenerator.dll "-reports:.\web-api-tests\TestResults\MyTestOutput.coveragexml" "-targetdir:.\web-api-tests\TestResults\coveragereport"
+
+    write-host 'CoverageReport Published'  -ForegroundColor Green
 
 
-dotnet C:\tools\.store\dotnet-reportgenerator-globaltool\4.5.8\dotnet-reportgenerator-globaltool\4.5.8\tools\netcoreapp3.0\any\ReportGenerator.dll "-reports:.\web-api-tests\TestResults\MyTestOutput.coveragexml" "-targetdir:.\web-api-tests\TestResults\coveragereport"
+    dotnet C:\sonar-scanner-msbuild-core2.0\SonarScanner.MSBuild.dll begin /k:"DotNetCoreWebAPI-Project" `
+      /d:sonar.host.url="http://localhost:9000" `
+      /d:sonar.cs.opencover.reportsPaths="./web-api-tests/TestResults/opencover.xml" `
+      /d:sonar.cs.vscoveragexml.reportsPaths="./web-api-tests/TestResults/MyTestOutput.coveragexml" `
+      /d:sonar.coverage.exclusions="**/*Test.cs" 
 
-write-host 'CoverageReport Published'  -ForegroundColor Green
 
-
-dotnet C:\sonar-scanner-msbuild-core2.0\SonarScanner.MSBuild.dll begin /k:"DotNetCoreWebAPI-Project" `
-/d:sonar.host.url="http://localhost:9000" `
-/d:sonar.cs.opencover.reportsPaths="./web-api-tests/TestResults/opencover.xml" `
-/d:sonar.cs.vscoveragexml.reportsPaths="./web-api-tests/TestResults/MyTestOutput.coveragexml" `
-/d:sonar.coverage.exclusions="**/*Test.cs" 
+    dotnet build .\web-api.sln
 
 
 dotnet C:\sonar-scanner-msbuild-core2.0\SonarScanner.MSBuild.dll end 
